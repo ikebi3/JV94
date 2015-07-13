af8fa74985799c4faab8834bce8b564fdc8fd0db
ykevi <ykevi94@gmail.com>
2015-07-13 21:15:51 +0900

    更新

--- java_mysql/DbConnect.java
+++ java_mysql/DbConnect.java
@@ -16,11 +16,34 @@ public class DbConnect {
 
 		Statement sta = con.createStatement();
 
-		String sql = "show databases;";
+		String sql = "select * from hage.user;";
+//		String sql = "select * from mysql.user;";
+		String sqlsub = "show culumns from hage.user;";
 
 		ResultSet rs = sta.executeQuery(sql);
+		ResultSet rs2 = sta.executeQuery(sqlsub);
 
-		rs.next();
-		System.out.println(rs.getString(1));
+		rs2.next();
+		System.out.println(rs2.getString(1)+"|");
+
+		while(rs.next()) {
+
+			System.out.print(rs.getString(1)+ "|" );
+			System.out.print(rs.getString(2)+ "|" );
+			System.out.print(rs.getString(3)+ "|" );
+			System.out.print(rs.getString(4)+ "|" );
+			System.out.print(rs.getString(5)+ "|" );
+			System.out.println(rs.getString(6)+ "|" );
+
+		}
+//		rs.beforeFirst();
+//		while(rs.next()) {
+//
+//			System.out.println(rs.getString(2)+ "|" );
+//		}
+
+//		rs.next();
+//		System.out.println(rs.getString(1));
+
 	}
 }
